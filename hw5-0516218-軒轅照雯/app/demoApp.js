032bac71d180123ca98d7d6ab216589bd245155e
winniehsuanyuan <starlightwinnie@gmail.com>
2020-04-10 09:25:45 +0800

    delete demoApp.js and modify demoNode.js

--- hw5-0516218-軒轅照雯/app/demoApp.js
+++
@@ -1,8 +0,0 @@
-module.exports.songInsert=function(){
-}
-
-module.exports.songDelete=function(){
-}
-
-module.exports.index=function(){
-}
--- hw5-0516218-軒轅照雯/app/demoNode.js
+++ hw5-0516218-軒轅照雯/app/demoNode.js
@@ -7,7 +7,6 @@
 //http://localhost/index.html
 
 var server=require("./demoServer.js"); //accept http request
-var app=require("./demoApp.js");
 
 server.init();
 server.init(8080);
